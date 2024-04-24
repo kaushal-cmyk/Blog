20dce4b325f7d7b3ac27c06c579498fc654787ba
Kaushal Adhikari <kaushaladhikari098@gmail.com>
2024-04-24 14:09:05 +0545

    changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const Home = () => {
+  let [blogs, setBlog] = useState([])
+
+  let getAllBlogs = async ()=> {
+    let result = await axios({
+      url: "http://localhost:8000/blogs",
+      method: "get"
+    })
+    setBlog (result.data.result)
+  }
+
+  useEffect(()=> {
+    getAllBlogs()
+  }, [])
+
+
+  // view blog info 
+  let blogInfo = blogs.map((item, i) => {
+    return (
+      <div key = {i} style = {{border : "solid red 2px",  marginBottom : "20px "}}>
+        <p>Title: {item.title}</p>
+        <p>Content: {item.content}</p>
+      </div>
+    )
+  })
   return (
-    <div>Home</div>
+    <div>
+      {blogInfo}
+    </div>
   )
 }
 
